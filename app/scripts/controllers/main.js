70dd2ee91f1af601ada4a7d25fb52e4e9cd504dc
KD-Chang <ibelieveicaner@gmail.com>
2014-07-21 22:34:04 +0800

    init the yo angular project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name drfindApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the drfindApp
+ */
+angular.module('drfindApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
