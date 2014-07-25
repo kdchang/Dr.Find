f398fc0fe726afd92b82149791aca3240adc8b05
Neson <neson@dex.tw>
2014-07-25 09:19:08 +0800

    working on search results layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'google-maps'
+    'google-maps',
+    'ui.bootstrap'
   ])
   .config(function ($routeProvider) {
     $routeProvider
