19fc30bf212475adec2bb5fdf4282c04134ad359
Michael Hsu (徐承志) <evenchange4@gmail.com>
2014-07-26 14:36:44 +0800

    get iii Token

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the drfindApp
  */
 angular.module('drfindApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $q, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -22,4 +22,33 @@ angular.module('drfindApp')
       },
       zoom: 8
     };
+
+    $scope.token = "";
+    $scope.getToken = function(){
+      var q = $scope.q = $q.defer();
+      var p = $scope.q.promise;
+
+      var xsrf = $.param({
+        id: "d93f4a40f62804f081c3c53a22a1c4d3",
+        secret_key: "518ccaebd250af3e223739b216c4cac8"
+        });
+      var request = $http({
+        method: "POST",
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        url: "http://api.ser.ideas.iii.org.tw/api/user/get_token",
+        data: xsrf
+      });
+
+      request.success(function(data, status, headers, config) {
+        q.resolve(data.result.token);
+      });
+
+      p.then(function(token){
+        console.log(token);
+        $scope.token = token;
+      });
+    };
+
+    $scope.getToken();
+
   });
