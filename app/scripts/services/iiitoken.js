772039e2d33223238d362331e7ee01d59a6698ad
Michael Hsu (徐承志) <evenchange4@gmail.com>
2014-07-26 12:59:51 +0800

    token iii init

---
+++ app/scripts/services/iiitoken.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name drfindApp.iiitoken
+ * @description
+ * # iiitoken
+ * Service in the drfindApp.
+ */
+angular.module('drfindApp')
+  .service('Iiitoken', function Iiitoken() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
